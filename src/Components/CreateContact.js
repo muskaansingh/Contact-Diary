6cca3617d873cc83d86cf546090fa5bf4b99cb5b
Muskaan <muskaansingh235@gmail.com>
2021-04-21 12:16:39 +0530

    created createContact form

--- src/Components/CreateContact.js
+++ src/Components/CreateContact.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const CreateContact = () => {
-    return (
-        <div>
-            <h1>CREATE CONTACT!</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>CREATE CONTACT!</h1>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Enter your name" />
+        </Form.Group>
 
-export default CreateContact
+        <Form.Group controlId="formBasicEmail">
+        <Form.Label>Phone No.</Form.Label>
+        <Form.Control type="phone" placeholder="Enter your Phone No." />
+      </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
+
+        <Button variant="danger" type="submit">
+          Create Contact
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateContact;
